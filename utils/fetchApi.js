403a82f1efa8037cfbd78892fb1fc24d4317f009
Laxmi Mahato <63464997+implaxmi@users.noreply.github.com>
2022-04-15 18:56:53 +0545

    Update fetchApi.js
    Api file edited

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -7,7 +7,7 @@ export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-            'X-RapidAPI-Key': '4e8282d886msh8694c671f019a63p11100bjsndc5b64f4dc95'
+            'X-RapidAPI-Key': ''
           }
     });
 
